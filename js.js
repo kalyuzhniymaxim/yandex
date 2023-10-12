b6cbd2240b83e226d4a31e7fa511dfeaf62dbca3
kalyuzhniymaxim <124453518+kalyuzhniymaxim@users.noreply.github.com>
2023-10-12 21:49:57 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,40 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    center: true,
+    dots: true,
+    nav:true,
+    navText: ["<img src='Group 120867.png'>", "<img src='Group 120861.png'>"],
+    autoplay:true,
+    autoplayTimeout:4000000,
+    autoplayHoverPause:false,
+    responsive: {
+    0: {
+        items: 1,
+    },
+    800: {
+        items: 3,
+    },
+    },
+});
+
+//    $('.owl-carousel').owlCarousel({
+//        margin: 50,
+//        responsiveClass: true,
+//        items: 2,
+//        nav: true,
+//        navText: ["<img src='img/prev.png'>", "<img src='img/next.png'>"],
+//        responsive: {
+//            0: {
+//                items: 1,
+//                margin: 150,
+//                center: true,
+//                autoWidth: true,
+//                nav: false,
+//                navText: []
+//            },
+//            992: {
+//                items: 2
+//            }
+//        }
+//    });
